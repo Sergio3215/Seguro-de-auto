3d2b01f4a3365a3b75d8f9415f4e56b952df47a5
Sergio <principiante.en.programar@gmail.com>
2022-08-27 05:58:06 -0300

    init the project

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace InsuranceCar_WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InsuranceCar_WebAPI.WebApiApplication" Language="C#" %>
